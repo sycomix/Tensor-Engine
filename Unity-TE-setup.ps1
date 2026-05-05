d1552360220870c2f0235ac0b3dd5dffa6996e66
Del <6117352+sycomix@users.noreply.github.com>
2026-05-05 05:54:19 -0500

    [MWX Checkpoint] Before editing i:\Tensor-Engine\Unity-TE-setup.ps1 (2026-05-05T10-54-18-904Z)
    Former-commit-id: 3175757f39ddd09bc50aa40b1c7126ed9d5324c7

--- Unity-TE-setup.ps1
+++ Unity-TE-setup.ps1
@@ -95,7 +95,7 @@ function Invoke-CommandWithExitCode($command, $commandArgs, $workingDir = $null)
     }
     $exitCode = 0
     try {
-        $null = & $command @commandArgs 2>&1
+        & $command @commandArgs
         $exitCode = $LASTEXITCODE
     } catch {
         $exitCode = 1
