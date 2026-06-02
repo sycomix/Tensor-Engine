d83af2082bf7c5a8a38d3356cd1236da5b9fbf21
Del <6117352+sycomix@users.noreply.github.com>
2026-06-02 11:43:30 -0500

    [MWX Checkpoint] Before editing src/io/image.rs (2026-06-02T16-43-29-601Z)
    Former-commit-id: 0d326c55986f19f0bc26d2ee3f4f0e5c1dbb5c5c

--- src/io/tokenizers.rs
+++ src/io/tokenizers.rs
@@ -347,7 +347,7 @@ impl TiktokenTokenizer {
             while i < tokens.len() - 1 {
                 let pair = (tokens[i].clone(), tokens[i + 1].clone());
                 if let Some(&merged_id) = self.merges.iter().find(|(l, r)| (*l, *r) == pair) {
-                    // Find the merged token id from merges (the second element is the new symbol)
+                    // Find the merged token in vocabulary by the new symbol from merges
                     let new_symbol = &self.merges[merged_id.1];
                     if let Some(&token_id) = self.vocab.get(new_symbol) {
                         tokens.splice(i..=i + 1, vec![format!("<0x{:02X}>", token_id)]);
