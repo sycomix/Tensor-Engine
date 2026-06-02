540edab6b2de03c98ba267e272d2c57090d75a27
Del <6117352+sycomix@users.noreply.github.com>
2026-06-02 11:47:53 -0500

    [MWX Checkpoint] Before editing src/nn/text_preprocessing.rs (2026-06-02T16-47-52-919Z)

--- src/nn/pruning.rs
+++ src/nn/pruning.rs
@@ -158,7 +158,7 @@ impl Pruner {
         // Fisher-Yates shuffle for first num_zeros elements
         let mut rng = rand::rng();
         for i in 0..num_zeros {
-            let j = i + (rng.random::<usize>() % (total - i));
+            let j = i + rng.random_range(0..(total - i));
             indices.swap(i, j);
         }
 
