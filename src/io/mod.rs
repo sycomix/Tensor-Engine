5ac8915aaa7c1bb832db86f41b87c6f7b9a276a4
Del <6117352+sycomix@users.noreply.github.com>
2026-06-01 23:58:35 -0500

    [MWX Checkpoint] Before creating src/nn/audio_models.rs (2026-06-02T04-58-34-800Z)

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -4,20 +4,16 @@ pub mod safetensors_loader;
 #[cfg(not(feature = "safe_tensors"))]
 // placeholder module to avoid missing module issues when feature disabled
 mod safetensors_loader {}
-#[cfg(feature = "with_tokenizers")]
+
 pub mod tokenizers;
-#[cfg(not(feature = "with_tokenizers"))]
-mod tokenizers {}
 
 #[cfg(feature = "with_tch")]
 pub mod pytorch_loader;
 #[cfg(not(feature = "with_tch"))]
 mod pytorch_loader {}
 
-#[cfg(feature = "vision")]
 pub mod image;
-#[cfg(not(feature = "vision"))]
-mod image {}
+
 #[cfg(feature = "vision")]
 pub mod image_text_dataloader;
 #[cfg(not(feature = "vision"))]
@@ -27,6 +23,7 @@ mod image_text_dataloader {}
 pub mod audio;
 #[cfg(not(feature = "audio"))]
 mod audio {}
+
 #[cfg(feature = "audio")]
 pub mod dataloader;
 #[cfg(not(feature = "audio"))]
