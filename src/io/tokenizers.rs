364412c7d3bd4b1becbe1ce4b269c17814421006
Del <6117352+sycomix@users.noreply.github.com>
2026-06-02 11:47:28 -0500

    [MWX Checkpoint] Before editing src/nn/pruning.rs (2026-06-02T16-47-27-495Z)

--- src/io/tokenizers.rs
+++ src/io/tokenizers.rs
@@ -348,7 +348,7 @@ impl TiktokenTokenizer {
                 let pair = (tokens[i].clone(), tokens[i + 1].clone());
                 if let Some(merged_id) = self.merges.iter().position(|(l, r)| (*l, *r) == pair) {
                     // Find the merged token in vocabulary by the new symbol from merges
-                    let new_symbol = self.merges[merged_id].new_symbol.clone();
+                    let new_symbol = self.merges[merged_id].1.clone();
                     if let Some(&token_id) = self.vocab.get(&new_symbol) {
                         tokens.splice(i..=i + 1, vec![format!("<0x{:02X}>", token_id)]);
                         changed = true;
