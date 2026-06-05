3ee7ba2f24cdc367e0c08ca0d3bbc0417626bd11
Del <6117352+sycomix@users.noreply.github.com>
2026-06-05 02:05:42 -0500

    [MWX Checkpoint] Before editing src/nn/audio_augmentation.rs (2026-06-05T07-05-42-300Z)

--- src/nn/audio_augmentation.rs
+++ src/nn/audio_augmentation.rs
@@ -253,7 +253,7 @@ impl AudioNormalizer {
         }
 
         Tensor::new(
-            match ArrayD::from_shape_vec(IxDyn(&shape), out.into_raw_vec()) {
+            match ArrayD::from_shape_vec(IxDyn(&shape), out) {
                 Ok(v) => v,
                 Err(_) => ArrayD::zeros(IxDyn(&shape)),
             },
